8e16be44317f494be325a7f6923400b0dd39c5a6
Jesse Kipp and J <esbe.v2@gmail.com jfk303@nyu.edu>
2013-09-25 10:59:15 -0400

    Displays disks via jquery

--- hanoiUI.js
+++ hanoiUI.js
@@ -1,11 +1,26 @@
 var game = new Hanoi.Game();
 game.run();
 
-var render = function() {
+var clearBoard = function() {
 
+}
+
+var render = function() {
+  clearBoard();
+  game.towers.forEach(function(tower, tower_index) {
+    // each tower
+    reversed_tower = tower.reverse();
+    reversed_tower.forEach(function(disk) {
+      // each disk in tower
+      newDisk = $('<div class="disk"></div>');
+      newDisk.css('width', '' + disk*60 + 'px');
+      $('#tower'+tower_index).append(newDisk);
+    });
+  });
 }
 
 
 $(document).ready( function() {
-  $("div.tower").click()
+  render();
+  // $("div.tower").click()
 })
