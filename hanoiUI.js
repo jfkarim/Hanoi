428021b53646fa4efd185f6a6fde3dca39b299a9
Jesse Kipp and J <esbe.v2@gmail.com jfk303@nyu.edu>
2013-09-25 13:29:28 -0400

    Adds turn counter

--- hanoiUI.js
+++ hanoiUI.js
@@ -1,44 +1,51 @@
-var game = new Hanoi.Game();
-game.run();
-
-var startTowerIndex = null;
-
-var clearBoard = function() {
-  $('.disk').remove();
-}
-
-var render = function() {
-  clearBoard();
-  game.towers.forEach(function(tower, tower_index) {
-    // each tower
-    reversed_tower = tower.slice();
-    reversed_tower.reverse();
-    reversed_tower.forEach(function(disk) {
-      // each disk in tower
-      newDisk = $('<div class="disk"></div>');
-      newDisk.css('width', '' + disk*60 + 'px');
-      $('#'+tower_index).append(newDisk);
-    });
-  });
-}
-
-
 $(document).ready( function() {
+  var $counter = $('#status');
+  var game = new Hanoi.Game();
+  var startTowerIndex = null;
+  var turns = 0;
+  game.run();
+
+  var clearBoard = function() {
+    $('.disk').remove();
+  }
+
+  var render = function() {
+    clearBoard();
+    if (turns === 1) {
+      $counter.html(''+ turns + " Move");
+    } else {
+      $counter.html(''+ turns + " Moves");
+    }
+    game.towers.forEach(function(tower, tower_index) {
+      // each tower
+      reversed_tower = tower.slice();
+      reversed_tower.reverse();
+      reversed_tower.forEach(function(disk) {
+        // each disk in tower
+        newDisk = $('<div class="disk"></div>');
+        newDisk.css('width', '' + disk*60 + 'px');
+        $('#'+tower_index).append(newDisk);
+      });
+    });
+  }
+
   render();
   $("div.tower").click(function() {
     if (startTowerIndex !== null) {
       // if we've already picked up a disc
       var endTowerIndex = parseInt($(this).attr('id'));
       if (game.move(startTowerIndex,endTowerIndex)) {
+        turns++;
         render();
       } else {
         console.log("Invalid Move")
+        $('.selected').removeClass('selected');
       }
-
       startTowerIndex = null;
     } else {
       // if we haven't picked up a disc
       startTowerIndex = parseInt($(this).attr('id'));
+      $(this).children('.disk').first().addClass('selected');
     }
   });
 });
